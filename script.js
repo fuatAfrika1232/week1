f8469c66476625c2f420c3fed5ed652640aed72e
fuatAfrika1232 <89730943+fuatAfrika1232@users.noreply.github.com>
2021-09-14 22:26:58 +0100

    bot

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("merhaba fuat");
+import numb as np
+
+class MuscularGreenPenguin(QCAlgorithm):
+
+    def Initialize(self):
+     self.setcash(10000)
+
+     Self.setstartDate(2021,9.1)
+     Self.setEndDate(2022,9,1)
+
+     Self.symbol = Self.AddEquity("spy,Resulution.daily").symbol
+     
+     Self.lookback = 20
+     
+     Self.ceiling, self.floor = 30,10
+     
+     Self.initialStopRisk = 0.98
+     self.trailingStopRisk = 0.9
+     
+     
+         
+
+ def OnData(self, data):
+     Self.Plot("data chart",Self.Symbol,Self.Securities[Self.Symbol].close)
